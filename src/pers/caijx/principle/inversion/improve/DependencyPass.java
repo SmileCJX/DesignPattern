0ffdc086c30153ba562d510fa843a6575a40a43f
SmileCJX <1092925421@qq.com>
2019-10-04 11:44:56 +0800

    通过构造方法传递依赖

--- src/pers/caijx/principle/inversion/improve/DependencyPass.java
+++ src/pers/caijx/principle/inversion/improve/DependencyPass.java
@@ -14,24 +14,49 @@ public class DependencyPass {
     }
 }
 
-interface ITV { // ITV接口
+///**
+// * 方式1： 通过接口传递实现依赖
+// * 开关的接口
+// */
+//interface IOpenAndClose {
+//
+//    void open(ITV itv); // 抽象方法，接收接口
+//}
+//
+//interface ITV { // ITV接口
+//
+//    void play();
+//}
+//
+//class OpenAndClose implements IOpenAndClose {
+//
+//    @Override
+//    public void open(ITV tv) {
+//        tv.play();
+//    }
+//}
+
+// 方式2：通过构造方法传递依赖
+interface IOpenAndClose {
+
+    void open();
+}
+
+interface ITV {
 
     void play();
 }
 
-/**
- * 方式1： 通过接口传递实现依赖
- * 开关的接口
- */
-interface IOpenAndClose {
-
-    void open(ITV itv); // 抽象方法，接收接口
-}
-
 class OpenAndClose implements IOpenAndClose {
 
+    private ITV tv;  // 成员
+
+    public OpenAndClose(ITV tv) {  // 构造器
+        this.tv = tv;
+    }
+
     @Override
-    public void open(ITV tv) {
-        tv.play();
+    public void open() {
+        this.tv.play();
     }
 }
