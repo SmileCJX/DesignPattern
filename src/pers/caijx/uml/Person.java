3518c4b7fedf45acd4be72bf30a7ec93c40988e3
SmileCJX <1092925421@qq.com>
2019-10-06 16:35:57 +0800

    UML Person类

---
+++ src/pers/caijx/uml/Person.java
@@ -0,0 +1,31 @@
+package pers.caijx.uml;
+
+/**
+ * @ClassName Person
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/6
+ * @Version V1.0
+ **/
+public class Person {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
