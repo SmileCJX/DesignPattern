7b1c6f7231bd5bc8f798d18b0d72114edb7b496f
SmileCJX <1092925421@qq.com>
2019-10-07 16:42:46 +0800

    泛化类图

---
+++ src/pers/caijx/uml/generalization/DaoSupport.java
@@ -0,0 +1,19 @@
+package pers.caijx.uml.generalization;
+
+/**
+ * @ClassName DaoSupport
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/7
+ * @Version V1.0
+ **/
+public abstract class DaoSupport {
+
+    public void save(Object entity) {
+
+    }
+
+    public void delete(Object id) {
+
+    }
+}
---
+++ src/pers/caijx/uml/generalization/PersonServiceBean.java
@@ -0,0 +1,11 @@
+package pers.caijx.uml.generalization;
+
+/**
+ * @ClassName PersonServiceBean
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/7
+ * @Version V1.0
+ **/
+public class PersonServiceBean extends DaoSupport {
+}
