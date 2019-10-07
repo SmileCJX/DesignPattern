00ea3967cceb9944d1ecc058269a74a02fbf1dbf
SmileCJX <1092925421@qq.com>
2019-10-07 18:27:30 +0800

    组合关系和聚合关系类图

---
+++ src/pers/caijx/uml/composition/Head.java
@@ -0,0 +1,11 @@
+package pers.caijx.uml.composition;
+
+/**
+ * @ClassName Head
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/7
+ * @Version V1.0
+ **/
+public class Head {
+}
---
+++ src/pers/caijx/uml/composition/IDCard.java
@@ -0,0 +1,11 @@
+package pers.caijx.uml.composition;
+
+/**
+ * @ClassName IDCard
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/7
+ * @Version V1.0
+ **/
+public class IDCard {
+}
