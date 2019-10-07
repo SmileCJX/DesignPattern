134aff49511d0335f54f7145eff1b04b384de5a5
SmileCJX <1092925421@qq.com>
2019-10-07 17:38:10 +0800

    双向一对一关联类图

---
+++ src/pers/caijx/uml/relationship/IDCard.java
@@ -0,0 +1,13 @@
+package pers.caijx.uml.relationship;
+
+/**
+ * @ClassName IDCard
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/7
+ * @Version V1.0
+ **/
+public class IDCard {
+
+    private Person person;
+}
---
+++ src/pers/caijx/uml/relationship/Person.java
@@ -0,0 +1,13 @@
+package pers.caijx.uml.relationship;
+
+/**
+ * @ClassName Person
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/7
+ * @Version V1.0
+ **/
+public class Person {
+
+    private IDCard idCard;
+}
