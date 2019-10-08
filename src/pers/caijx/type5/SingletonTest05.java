4e4342fa22cd28cdd9be87d767129a60007ff3ce
SmileCJX <1092925421@qq.com>
2019-10-08 21:11:06 +0800

    单例（同步代码块懒汉式），不能用，存在安全问题

---
+++ src/pers/caijx/type5/SingletonTest05.java
@@ -0,0 +1,40 @@
+package pers.caijx.type5;
+
+import java.sql.SQLOutput;
+
+/**
+ * @ClassName SingletonTest05
+ * @Description: 单例（同步代码块懒汉式），不能用，存在安全问题
+ * @Author Think
+ * @Date 2019/10/8
+ * @Version V1.0
+ **/
+public class SingletonTest05 {
+
+    public static void main(String[] args) {
+        System.out.println("线程安全，同步代码块");
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);
+        System.out.println("singleton1.hashCode = " + singleton1.hashCode());
+        System.out.println("singleton2.hashCode = " + singleton2.hashCode());
+    }
+}
+
+class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (null == singleton) {
+            synchronized (Singleton.class) {
+                singleton = new Singleton();
+            }
+        }
+        return singleton;
+    }
+}
