aa36f100d77f6d8fce6b5096a995fb65b06909a9
SmileCJX <1092925421@qq.com>
2019-10-09 21:58:29 +0800

    披萨商店客户端

---
+++ src/pers/caijx/simplefactory/pizzastore/pizza/PizzaStore.java
@@ -0,0 +1,16 @@
+package pers.caijx.simplefactory.pizzastore.pizza;
+
+/**
+ * @ClassName PizzaStore
+ * @Description: TODO
+ * @Author Think
+ * @Date 2019/10/9
+ * @Version V1.0
+ **/
+// 相当于一个客户端，发出订购
+public class PizzaStore {
+
+    public static void main(String[] args) {
+        new OrderPizza();
+    }
+}
