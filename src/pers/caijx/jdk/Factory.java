014f0a249f5a7175868e233adeab8a93c4362c33
SmileCJX <1092925421@qq.com>
2019-10-15 21:26:56 +0800

    简单工厂

--- src/pers/caijx/jdk/Factory.java
+++ src/pers/caijx/jdk/Factory.java
@@ -12,7 +12,7 @@ import java.util.Calendar;
 public class Factory {
 
     public static void main(String[] args) {
-        // getInstance 是
+        // getInstance 是 Calendar的静态方法
         Calendar cal = Calendar.getInstance();
         // 注意月份下标要从0开始，所以取月份 + 1
         System.out.println("年：" + cal.get(Calendar.YEAR));
