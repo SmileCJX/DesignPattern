1e8e762ffaea9c14ebcc002b5eac72504c4ea200
SmileCJX <1092925421@qq.com>
2019-10-15 21:55:51 +0800

    使用原型模式的创建方式

---
+++ src/pers/caijx/prototype/impl/Client.java
@@ -0,0 +1,28 @@
+package pers.caijx.prototype.impl;
+
+/**
+ * @ClassName Client
+ * @Description: TODO
+ * @Author JunXiangCai
+ * @Date 2019/10/15
+ * @Version V1.0
+ **/
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("原型模式完成对象的创建");
+        Sheep sheep = new Sheep("tom",1,"白色");
+
+        Sheep sheep1 = (Sheep) sheep.clone();
+        Sheep sheep2 = (Sheep) sheep.clone();
+        Sheep sheep3 = (Sheep) sheep.clone();
+        Sheep sheep4 = (Sheep) sheep.clone();
+        Sheep sheep5 = (Sheep) sheep.clone();
+
+        System.out.println("sheep1 = " + sheep1);
+        System.out.println("sheep2 = " + sheep2);
+        System.out.println("sheep3 = " + sheep3);
+        System.out.println("sheep4 = " + sheep4);
+        System.out.println("sheep5 = " + sheep5);
+    }
+}
