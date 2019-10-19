f682faba617abfca433bec18d8d2cc9e4c6f6240
SmileCJX <1092925421@qq.com>
2019-10-20 00:07:43 +0800

    建造指挥者

---
+++ src/pers/caijx/builder/improve/HouseDirector.java
@@ -0,0 +1,32 @@
+package pers.caijx.builder.improve;
+
+/**
+ * @ClassName HouseDirector
+ * @Description: 指挥者，这里指定制作流程，返回房子
+ * @Author JunXiangCai
+ * @Date 2019/10/19
+ * @Version V1.0
+ **/
+public class HouseDirector {
+
+    HouseBuilder houseBuilder = null;
+
+    // 通过构造器传入 houseBuilder
+
+    public HouseDirector(HouseBuilder houseBuilder) {
+        this.houseBuilder = houseBuilder;
+    }
+
+    // 通过set方法 传入houseBuilder，如果想要修改，通过此方法
+    public void setHouseBuilder(HouseBuilder houseBuilder) {
+        this.houseBuilder = houseBuilder;
+    }
+
+    // 如何处理建造房子的流程，交给指挥者
+    public House constructHouse() {
+        houseBuilder.buildBasic();
+        houseBuilder.buildWalls();
+        houseBuilder.roofed();
+        return houseBuilder.buildHouse();
+    }
+}
