27b01a93818ba7c6ab85bcda47b916cbd6bd7f10
SmileCJX <1092925421@qq.com>
2019-10-20 00:06:42 +0800

    建造者产品

---
+++ src/pers/caijx/builder/improve/House.java
@@ -0,0 +1,41 @@
+package pers.caijx.builder.improve;
+
+/**
+ * @ClassName House
+ * @Description: 产品--》Product
+ * @Author JunXiangCai
+ * @Date 2019/10/19
+ * @Version V1.0
+ **/
+public class House {
+
+    private String basic;
+
+    private String wall;
+
+    private String roofed;
+
+    public String getBasic() {
+        return basic;
+    }
+
+    public void setBasic(String basic) {
+        this.basic = basic;
+    }
+
+    public String getWall() {
+        return wall;
+    }
+
+    public void setWall(String wall) {
+        this.wall = wall;
+    }
+
+    public String getRoofed() {
+        return roofed;
+    }
+
+    public void setRoofed(String roofed) {
+        this.roofed = roofed;
+    }
+}
