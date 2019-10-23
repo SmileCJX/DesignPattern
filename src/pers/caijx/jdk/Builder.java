8e53d0ebadc071814cd08383b58e0be6be7402b5
SmileCJX <1092925421@qq.com>
2019-10-23 16:49:15 +0800

    建造者模式

---
+++ src/pers/caijx/jdk/Builder.java
@@ -0,0 +1,16 @@
+package pers.caijx.jdk;
+
+/**
+ * @ClassName Builder
+ * @Description: TODO
+ * @Author JunXiangCai
+ * @Date 2019/10/19
+ * @Version V1.0
+ **/
+public class Builder {
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Hello,world");
+        System.out.println(stringBuilder);
+    }
+}
