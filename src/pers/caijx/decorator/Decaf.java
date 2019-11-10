fcede651f3bdd61b34f833bef5ed17bbd8ef5032
SmileCJX <1092925421@qq.com>
2019-11-11 00:48:32 +0800

    单品咖啡

---
+++ src/pers/caijx/decorator/Decaf.java
@@ -0,0 +1,16 @@
+package pers.caijx.decorator;
+
+/**
+ * @ClassName Decaf
+ * @Description: TODO
+ * @Author JunXiangCai
+ * @Date 2019/11/10
+ * @Version V1.0
+ **/
+public class Decaf extends Coffee {
+
+    public Decaf() {
+        setDes(" Decaf ");
+        setPrice(3.0F);
+    }
+}
