bf890d2ca6733f00512c88d97afb028d487f9449
SmileCJX <1092925421@qq.com>
2019-11-14 18:50:06 +0800

    组织主键

---
+++ src/pers/caijx/composite/OrganizationComponent.java
@@ -0,0 +1,62 @@
+package pers.caijx.composite;
+
+/**
+ * @ClassName OrganizationComponent
+ * @Description: 组织主键
+ * @Author JunXiangCai
+ * @Date 2019/11/14
+ * @Version V1.0
+ **/
+public abstract class OrganizationComponent {
+
+    /**
+     * 名字
+     */
+    private String name;
+
+    /**
+     * 说明
+     */
+    private String des;
+
+    protected void add(OrganizationComponent organizationComponent) {
+        // 默认实现
+        throw new UnsupportedOperationException();
+    }
+
+    protected void remove(OrganizationComponent organizationComponent) {
+        // 默认实现
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * 构造器
+     * @param name
+     * @param des
+     */
+    public OrganizationComponent(String name, String des) {
+        this.name = name;
+        this.des = des;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    /**
+     * 方法print，做成抽象的，子类都需要实现
+     */
+    protected abstract void print();
+}
